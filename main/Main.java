7686a571dbd13d5c756bfee2bb3c5526c2c4ad65
Sergey Sosnovskiy <38312485+Sergey240390@users.noreply.github.com>
2018-04-12 11:52:03 +0300

    Create Main.java

---
+++ main/Main.java
@@ -0,0 +1,24 @@
+package Main;
+
+
+import Servlets.FirstServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        final String requestedFile = "/mirror";
+        final FirstServlet firstServlet = new FirstServlet();
+        context.addServlet(new ServletHolder(firstServlet), requestedFile);
+
+        final Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        System.out.println("Server started");
+        server.join();
+    }
+}
